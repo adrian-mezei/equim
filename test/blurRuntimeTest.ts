28b5c4ca2383a830d132e9dfed55753292f986f5
Mezei Adrián <mezei.adrian@gmail.com>
2019-10-06 17:41:16 +0200

    Bugfix of unused imports in tests.

--- test/blurRuntimeTest.ts
+++ test/blurRuntimeTest.ts
@@ -1,4 +1,3 @@
-import { expect, assert } from 'chai';
 import { Equim } from '../src/Equim';
 import Jimp = require('jimp');
 
--- test/equimReadWriteTest.ts
+++ test/equimReadWriteTest.ts
@@ -1,4 +1,4 @@
-import { expect, assert } from 'chai';
+import { expect } from 'chai';
 import { Equim } from '../src/Equim';
 import Jimp = require('jimp');
 
